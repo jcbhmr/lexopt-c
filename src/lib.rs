7f5ec0cbed5659c5ac3516173c04a3edc4de5d28
Jacob Hummer <jcbhmr@outlook.com>
2024-11-07 21:38:01 +0000

    Pending changes exported from your codespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
-#![allow(non_camel_case_types)]
-
-mod prelude;
-mod parser;
-mod raw_args;
-mod values_iter;
-mod arg;
-mod error;
-
-pub use crate::prelude::*;
-// pub use crate::parser::*;
-// pub use crate::raw_args::*;
-// pub use crate::values_iter::*;
-pub use crate::arg::*;
-pub use crate::error::*;
+#[no_mangle]
+pub extern "C" fn lexopt_parser_next(parser: *mut lexopt::Parser, out_arg: *mut lexopt::Arg) -> *const lexopt::Error {
+    let parser = unsafe {parser.as_mut()}.expect("parser is null");
+    let out_arg = unsafe{out_arg.as_mut()}.expect("out_arg is null");
+    match parser.next() {
+        Ok(Some(arg)) => {
+            *out_arg = arg;
+            std::ptr::null()
+        }
+        Ok(None) => std::ptr::null(),
+        Err(err) => Box::into_raw(Box::new(err)),
+    }
+}
--- src/parser.rs
+++
@@ -1,4 +0,0 @@
-#[no_mangle]
-pub extern "C" fn lexopt_parser_next(parser: *mut lexopt::Parser, out_arg) -> *const crate::lexopt_error {
-
-}
